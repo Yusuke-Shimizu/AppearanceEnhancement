d0984b5f369106c00e2a9fea9aab161272e61def
Yusuke-Shimizu <shimi.nekoze@gmail.com>
2013-03-11 15:33:48 +0900

    Initial Commit

---
+++ cameraBase03/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cameraBase03
+//
+//  Created by 志水 友輔 on 2013/03/11.
+//  Copyright (c) 2013年 志水 友輔. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
